18de94ce2dea98d4bd3886b4d0fa174a5085d91d
JIHOON <yjhoon0618@naver.com>
2023-09-19 23:47:30 +0900

    FSD를 이용한 전화번호 표시기

--- GccApplication9/GccApplication9/main.c
+++ GccApplication9/GccApplication9/main.c
@@ -1,10 +1,3 @@
-/*
- * GccApplication9.c
- *
- * Created: 2023-09-18 오후 10:22:40
- * Author : yjhoo
- */ 
-
 #include <avr/io.h>
 #define F_CPU 16000000UL
 #include<util/delay.h>
@@ -12,20 +5,19 @@
 int main(void)
 {
 	char tel_num[20] = {0x00, 0x00, 0x00, 0x00, 0x3f, 0x06, 0x3f, 0x40, 0x27, 0x6D, 0x6F, 0x4F, 0x40, 0x3F, 0x6D, 0x6D, 0x3F, 0x00, 0x00, 0x00};
-    
-    int i, j;
+	
+	int i, j;
 	DDRC = 0xff;
 	DDRG = 0x0f;
 	while(1){
 		for(i=0; i<17; i++){
 			for(j=0; j<50; j++){
-				PORTC = tel_num[i]; PORTG = 0x08; _delay_ms(5);
-				PORTC = tel_num[i+1]; PORTG = 0x04; _delay_ms(5);
-				PORTC = tel_num[i+2]; PORTG = 0x02; _delay_ms(5);
-				PORTC = tel_num[i+3]; PORTG = 0x01; _delay_ms(5);
+				PORTC = tel_num[i]; PORTG = 0x08; _delay_ms(2.5);
+				PORTC = tel_num[i+1]; PORTG = 0x04; _delay_ms(2.5);
+				PORTC = tel_num[i+2]; PORTG = 0x02; _delay_ms(2.5);
+				PORTC = tel_num[i+3]; PORTG = 0x01; _delay_ms(2.5);
 			}
+			PORTC = 0x00; PORTG = 0x0f; _delay_ms(500);
 		}
 	}
-	
 }
-
